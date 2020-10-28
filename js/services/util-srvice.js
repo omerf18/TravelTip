97e47bbcd574e4dcec622c2226974c28ad94b4e7
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-10-28 20:00:00 +0200

    util service

---
+++ js/services/util-srvice.js
@@ -0,0 +1,15 @@
+'use strict';
+
+
+elMainContainer = document.querySelector('.main-container');
+elModal = document.querySelector('.input-name-container');
+function showModal(){
+    elMainContent.classList.add('hide')
+    elModal.classList.remove('hide');
+    elModal.classList.add('show');
+}
+function showMainContent(){
+    elMainContent.classList.add('hide')
+    elModal.classList.remove('hide');
+    elModal.classList.add('show');
+}
