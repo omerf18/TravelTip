77aeff409ee3fabaeee1b7360568c4e1755d3fe8
Dennis Nemirovski <dennisnemirovski@gmail.com>
2020-10-28 19:27:54 +0200

    storage service

---
+++ js/services/storage-sevice.js
@@ -0,0 +1,13 @@
+'use strict'
+
+function saveToStorage(key, val) {
+    var str = JSON.stringify(val);
+    localStorage.setItem(key, str)
+}
+function loadFromStorage(key) {
+    var str = localStorage.getItem(key)
+    return JSON.parse(str)
+}
+function removeItemFromStorage(key){
+    localStorage.removeItem(key);
+}
